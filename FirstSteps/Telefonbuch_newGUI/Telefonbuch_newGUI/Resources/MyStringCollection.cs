a62e3e377873067354923ae91da58b9a15537ed8
KillBill <gr4ndpa@hotmail.com>
2016-03-22 15:47:55 +0100

    DummyClass with stored UserConfigFile -store Values, get Values

---
+++ FirstSteps/Telefonbuch_newGUI/Telefonbuch_newGUI/Resources/MyStringCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Telefonbuch.Resources
+{
+    class MyStringCollection
+    {
+        public static void MyStringCol()
+        { 
+
+            StringCollection MyStringCol = new StringCollection();
+            String[] myArray = new String[] { "NumberOfLines", "Line", "DownsAfterLine" };
+            MyStringCol.AddRange(myArray); //Copies the Array "myArray" to the End of the StringCollection "MyString"
+
+            Console.WriteLine("Initial contents of the StringCollection:");
+            PrintValues(MyStringCol);
+
+        }
+
+        public static void PrintValues(IEnumerable myStingCol)
+        {
+            foreach (Object obj in myStingCol)
+                Console.WriteLine("   {0}", obj);
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Testdummy.Reference.MyStringTest
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            
+            String[] myArray = new String[] { "NumberOfLines","7", "DownsAfterLine","1","2","1","3","3","1","1","1","3","3","1","1"};
+            //Set specific Value
+            myArray[3] = "3";
+            string Arraylength = (myArray.Length.ToString());
+            ExampleTest.Properties.Settings.Default.MyExampleFile.AddRange (myArray);
+            ExampleTest.Properties.Settings.Default.Save();
+
+            string Config = string.Join("\n", myArray);
+            MessageBox.Show(Config);
+
+
+            /*
+            StringCollection MyStringCol = ExampleTest.Properties.Settings.Default.MyExampleFile;
+            MessageBox.Show(MyStringCol[2]);*/
+
+
+            /*
+            StringCollection MyStringCol = ExampleTest.Properties.Settings.Default.MyExampleFile;
+            if (MyStringCol == null)
+            { MyStringCol = new StringCollection(); }
+            String[] myArray = new String[] { "NumberOfLines", "Line", "DownsAfterLine" };
+            MyStringCol.AddRange(myArray); //Copies the Array "myArray" to the End of the StringCollection "MyString"
+            MyStringCol.Add("+1");
+            
+            ExampleTest.Properties.Settings.Default.MyExampleFile = MyStringCol;
+            ExampleTest.Properties.Settings.Default.Save(); */
+
+        }
+     }
+}
