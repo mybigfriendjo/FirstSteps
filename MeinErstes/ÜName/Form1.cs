f13bd46d72aca378571872fb2b00d241ccfbd17e
Alpin <rpci9076@A860001E>
2016-09-07 10:24:41 +0200

    Added ForeColor/Backcolor

--- MeinErstes/ÜName/Form1.cs
+++ MeinErstes/ÜName/Form1.cs
@@ -12,25 +12,33 @@ namespace ÜName {
         private void btnPosRel_Click(object sender, EventArgs e) {
             btnTest.Location = new Point(btnTest.Location.X + 20, btnTest.Location.Y);
             LabelReferesh();
+            btnTest.BackColor = Color.Maroon;
         }
 
         private void btnPosAbs_Click(object sender, EventArgs e) {
             btnTest.Location = new Point(100, 200);
             LabelReferesh();
+            btnTest.BackColor = Color.FromArgb(192, 10, 23);
         }
 
         private void btnSizeRel_Click(object sender, EventArgs e) {
             btnTest.Size = new Size(btnTest.Size.Width + 20, btnTest.Size.Height + 20);
             LabelReferesh();
+            //Changes Control Background Color
+            btnTest.BackColor = Color.FromArgb(192, 50, 200);
         }
 
         private void btnSizeAbs_Click(object sender, EventArgs e) {
             btnTest.Size = new Size(50, 100);
             LabelReferesh();
+            //Changes Text Color
+            btnTest.ForeColor = Color.Fuchsia;
         }
 
         private void btnAnzeige_Click(object sender, EventArgs e) {
             LabelReferesh();
+            //Sets Form Background Color.
+            BackColor = Color.Goldenrod;
         }
 
         private void LabelReferesh() { 
