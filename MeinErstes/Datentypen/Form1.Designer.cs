8c86492d2fcfb142fcc9dc6a0b28720a5909bcf1
Alpin <rpci9076@A860001E>
2016-09-07 16:56:09 +0200

    Übung 2.2 Datentypen

---
+++ MeinErstes/Datentypen/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Datentypen {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnAnzeige = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAnzeige
+            // 
+            this.btnAnzeige.Location = new System.Drawing.Point(12, 12);
+            this.btnAnzeige.Name = "btnAnzeige";
+            this.btnAnzeige.Size = new System.Drawing.Size(73, 22);
+            this.btnAnzeige.TabIndex = 7;
+            this.btnAnzeige.Text = "Anzeige";
+            this.btnAnzeige.UseVisualStyleBackColor = true;
+            this.btnAnzeige.Click += new System.EventHandler(this.btnAnzeige_Click);
+            // 
+            // lblSize
+            // 
+            this.lblSize.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSize.Location = new System.Drawing.Point(12, 48);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(260, 317);
+            this.lblSize.TabIndex = 8;
+            this.lblSize.Text = "label1";
+            this.lblSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 374);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.btnAnzeige);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnzeige;
+        private System.Windows.Forms.Label lblSize;
+    }
+}
+
---
+++ MeinErstes/Datentypen/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datentypen {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnAnzeige_Click(object sender, EventArgs e) {
+            /* Ganze Zahlen */
+            byte By;
+            short Sh;
+            int It, Hex;
+            long Lg;
+
+            /* Zahlen mit Nachkommastellen */
+            float Fl;
+            double Db1, Db2, Exp1, Exp2;
+            decimal De;
+
+            /* Boolesche Variable, Zeichen,
+            Zeichenkette */
+            bool Bo;
+            char Ch;
+            string St;
+
+            /* Ganze Zahlen */
+            By = 200;
+            Sh = 30000;
+            It = 2000000000;
+            Lg = 3000000000;
+            Hex = 0x3a;
+
+            /* Zahlen mit Nachkommastellen */
+            Fl = 1.0f / 7;
+            Db1 = 1 / 7;
+            Db2 = 1.0 / 7;
+            De = 1.0m / 7;
+            Exp1 = 1.5e3;
+            Exp2 = 1.5e-3;
+
+            /* Boolesche Variable, Zeichen,
+            Zeichenkette */
+            Bo = true;
+            Ch = 'a';
+            St = "Zeichenkette";
+
+            lblSize.Text = "byte: " + By + "\n" + "short: " +
+            Sh + "\n" +
+            "int: " + It + "\n" + "long: " + Lg
+            + "\n" +
+            "(hexadezimale Zahl): " + Hex +
+            "\n\n" +
+            "float: " + Fl + "\n" + "double 1:" + Db1 + "\n" +
+            "double 2: " + Db2 + "\n" +
+            "decimal: " + De + "\n" +
+            "(Exponent positiv): " + Exp1 +
+            "\n" +
+            "(Exponent negativ): " + Exp2 +
+            "\n\n" +
+            "bool: " + Bo + "\n" +
+            "char: " + Ch + "\n" + "string: " +St;
+
+
+            // Multiple Variable + Value declaration Test
+
+            string i="Letter",o=i,u=o;
+            i = "Bomb";
+            //lblSize.Text = i + " " + o + " " + u;  //Result = Bomb Letter Letter
+
+
+            // Numbers can be stored as Hex as well > result is a Number
+            int a = 9654;
+            int b = 0x742;
+
+            //lblSize.Text = a + " " + b;
+
+            // 2.2 Übung Datentypen
+
+            string Adresse = "Claus Clever \n ";
+            int Alter = 32;
+
+            lblSize.Text = "Adresse: " + Adresse + "\n" + "Alter: " + Alter;
+        }
+    }
+}
