850a7e8a11123353896b3ba5c1d97037b7a91f15
Alpin <rpci9076@A860001E>
2016-09-15 17:02:02 +0200

    bla

--- MeinErstes/ÜSteuerbetrag/Form1.cs
+++ MeinErstes/ÜSteuerbetrag/Form1.cs
@@ -14,8 +14,13 @@ namespace ÜSteuerbetrag {
             InitializeComponent();
         }
 
+        double d = 30000 * 0.48;
+        double c = 29000 * 0.42;
+        double b = 13000 * 0.35;
+        double a = 7000 * 0.25;
+
         private void numericUpDown1_ValueChanged(object sender, EventArgs z) {
-            double Salary = Convert.ToDouble(numericUpDown1.Text);
+            double Salary = Convert.ToDouble(numericUpDown1.Value);
             lblTaxes.Text = "Steuern: ";
             lblNetto.Text = "Netto: ";
             lblYearsSalary.Text = "Jahresgehalt: ";
@@ -25,34 +30,24 @@ namespace ÜSteuerbetrag {
 
             if (Salary > 90000) {
                 double e = (Salary - 90000) * 0.50;
-                double d = 30000 * 0.48;
-                double c = 29000 * 0.42;
-                double b = 13000 * 0.35;
-                double a = 7000 * 0.25;
                 lblTaxes.Text += " " + (a + b + c + d + e);
                 lblNetto.Text += " " + (Salary - (a + b + c + d + e));
                 lblNettoMonth.Text += " " + (((Salary - (a + b + c + d + e))/14) - -(((Salary - (a + b + c + d + e)) / 14) * 0.058));
             }
             else if (Salary > 60000) {
                 double d = (Salary - 60000) * 0.48;
-                double c = 29000 * 0.42;
-                double b = 13000 * 0.35;
-                double a = 7000 * 0.25;
                 lblTaxes.Text += " " + (a + b + c + d);
                 lblNetto.Text += " " + (Salary - (a + b + c + d));
                 lblNettoMonth.Text += " " + (((Salary - (a + b + c + d)) / 14) - (((Salary - (a + b + c + d)) / 14) * 0.058));
             }
             else if (Salary > 31000) {
                 double c = (Salary - 29000) * 0.42;
-                double b = 13000 * 0.35;
-                double a = 7000 * 0.25;
                 lblTaxes.Text += " " + (a + b + c);
                 lblNetto.Text += " " + (Salary - (a + b + c));
                 lblNettoMonth.Text += " " + (((Salary - (a + b + c)) / 14) - (((Salary - (a + b + c)) / 14)*0.058));
             }
             else if (Salary > 18000) {
                 double b = (Salary - 18000) * 0.35;
-                double a = 7000 * 0.25;
                 lblTaxes.Text += " " + (a + b);
                 lblNetto.Text += " " + (Salary - (a + b));
                 lblNettoMonth.Text += " " + (((Salary - (a + b)) / 14) - ((Salary - (a + b)) / 14)*0.058);
