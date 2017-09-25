7001355e705c79b166adbc503b426e21d752ce73
Alpin <rpci9076@A860001E>
2017-09-25 14:48:59 +0200

    GuiChange

--- Planer/Planer/Planer.Designer.cs
+++ Planer/Planer/Planer.Designer.cs
@@ -28,29 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node1");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node2");
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node6");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node5", new System.Windows.Forms.TreeNode[] {
-            treeNode11});
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node7");
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Node4", new System.Windows.Forms.TreeNode[] {
-            treeNode12,
-            treeNode13});
-            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
-            treeNode14});
-            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
-            treeNode9,
-            treeNode10,
-            treeNode15});
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node6");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node5", new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node7");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node4", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode7});
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chkboxDone = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.tbDiscribtion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lbllbTask = new System.Windows.Forms.Label();
             this.lblDatagridTasksteps = new System.Windows.Forms.Label();
-            this.btnSaveToPacket = new System.Windows.Forms.Button();
             this.treeViewTasks = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSaveToPacket = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -60,9 +62,11 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.chkboxDone,
             this.tbDiscribtion});
-            this.dataGridView1.Location = new System.Drawing.Point(196, 39);
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 3);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 28);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(654, 286);
+            this.dataGridView1.Size = new System.Drawing.Size(598, 274);
             this.dataGridView1.TabIndex = 1;
             // 
             // chkboxDone
@@ -84,68 +88,97 @@
             // lbllbTask
             // 
             this.lbllbTask.AutoSize = true;
-            this.lbllbTask.Location = new System.Drawing.Point(23, 22);
+            this.lbllbTask.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbllbTask.Location = new System.Drawing.Point(3, 0);
             this.lbllbTask.Name = "lbllbTask";
-            this.lbllbTask.Size = new System.Drawing.Size(36, 13);
+            this.lbllbTask.Size = new System.Drawing.Size(252, 25);
             this.lbllbTask.TabIndex = 2;
             this.lbllbTask.Text = "Tasks";
             // 
             // lblDatagridTasksteps
             // 
             this.lblDatagridTasksteps.AutoSize = true;
-            this.lblDatagridTasksteps.Location = new System.Drawing.Point(208, 22);
+            this.lblDatagridTasksteps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDatagridTasksteps.Location = new System.Drawing.Point(261, 0);
             this.lblDatagridTasksteps.Name = "lblDatagridTasksteps";
-            this.lblDatagridTasksteps.Size = new System.Drawing.Size(56, 13);
+            this.lblDatagridTasksteps.Size = new System.Drawing.Size(195, 25);
             this.lblDatagridTasksteps.TabIndex = 3;
             this.lblDatagridTasksteps.Text = "Tasksteps";
             // 
-            // btnSaveToPacket
-            // 
-            this.btnSaveToPacket.Location = new System.Drawing.Point(211, 331);
-            this.btnSaveToPacket.Name = "btnSaveToPacket";
-            this.btnSaveToPacket.Size = new System.Drawing.Size(106, 23);
-            this.btnSaveToPacket.TabIndex = 4;
-            this.btnSaveToPacket.Text = "Save as Packet";
-            this.btnSaveToPacket.UseVisualStyleBackColor = true;
-            // 
             // treeViewTasks
             // 
-            this.treeViewTasks.Location = new System.Drawing.Point(12, 39);
+            this.treeViewTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewTasks.Location = new System.Drawing.Point(3, 28);
             this.treeViewTasks.Name = "treeViewTasks";
-            treeNode9.Name = "Node1";
-            treeNode9.Text = "Node1";
-            treeNode10.Name = "Node2";
-            treeNode10.Text = "Node2";
-            treeNode11.Name = "Node6";
-            treeNode11.Text = "Node6";
-            treeNode12.Name = "Node5";
-            treeNode12.Text = "Node5";
-            treeNode13.Name = "Node7";
-            treeNode13.Text = "Node7";
-            treeNode14.Name = "Node4";
-            treeNode14.Text = "Node4";
-            treeNode15.Name = "Node3";
-            treeNode15.Text = "Node3";
-            treeNode16.Name = "Node0";
-            treeNode16.Text = "Node0";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "Node1";
+            treeNode2.Name = "Node2";
+            treeNode2.Text = "Node2";
+            treeNode3.Name = "Node6";
+            treeNode3.Text = "Node6";
+            treeNode4.Name = "Node5";
+            treeNode4.Text = "Node5";
+            treeNode5.Name = "Node7";
+            treeNode5.Text = "Node7";
+            treeNode6.Name = "Node4";
+            treeNode6.Text = "Node4";
+            treeNode7.Name = "Node3";
+            treeNode7.Text = "Node3";
+            treeNode8.Name = "Node0";
+            treeNode8.Text = "Node0";
             this.treeViewTasks.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode16});
-            this.treeViewTasks.Size = new System.Drawing.Size(178, 604);
+            treeNode8});
+            this.tableLayoutPanel1.SetRowSpan(this.treeViewTasks, 4);
+            this.treeViewTasks.Size = new System.Drawing.Size(252, 604);
             this.treeViewTasks.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.003F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.33233F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.33233F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.33233F));
+            this.tableLayoutPanel1.Controls.Add(this.btnSaveToPacket, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.treeViewTasks, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblDatagridTasksteps, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbllbTask, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(862, 655);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // btnSaveToPacket
+            // 
+            this.btnSaveToPacket.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSaveToPacket.Location = new System.Drawing.Point(261, 308);
+            this.btnSaveToPacket.Name = "btnSaveToPacket";
+            this.btnSaveToPacket.Size = new System.Drawing.Size(195, 19);
+            this.btnSaveToPacket.TabIndex = 6;
+            this.btnSaveToPacket.Text = "Save as Packet";
+            this.btnSaveToPacket.UseVisualStyleBackColor = true;
             // 
             // Planer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(862, 655);
-            this.Controls.Add(this.treeViewTasks);
-            this.Controls.Add(this.btnSaveToPacket);
-            this.Controls.Add(this.lblDatagridTasksteps);
-            this.Controls.Add(this.lbllbTask);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Planer";
             this.Text = "Planer";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,8 +190,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn tbDiscribtion;
         private System.Windows.Forms.Label lbllbTask;
         private System.Windows.Forms.Label lblDatagridTasksteps;
-        private System.Windows.Forms.Button btnSaveToPacket;
         private System.Windows.Forms.TreeView treeViewTasks;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnSaveToPacket;
     }
 }
 
