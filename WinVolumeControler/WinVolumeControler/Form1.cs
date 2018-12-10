e0df97629084a2c9724e6bdd2b60803ce80815a8
Home <nmack@gmx.at>
2018-12-10 23:39:20 +0100

    Adjust Volume

---
+++ WinVolumeControler/WinVolumeControler/Form1.cs
@@ -0,0 +1,30 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinVolumeControler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void iwas()
+        {
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+            defaultPlaybackDevice.Volume = 80;
+
+        }
+    }
+}
