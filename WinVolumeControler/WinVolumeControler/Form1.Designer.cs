3d32625391aaa5041eb7860ee52068d8d5660a64
Home <gr4ndpa@hotmail.com>
2018-12-11 17:54:46 +0100

    Code works now, no HotKeys yet

--- WinVolumeControler/WinVolumeControler/Form1.Designer.cs
+++ WinVolumeControler/WinVolumeControler/Form1.Designer.cs
@@ -29,43 +29,72 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numUpDownVol1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numUpDownVol2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownVol1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownVol2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 30);
+            this.button1.Location = new System.Drawing.Point(11, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(99, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Set Volume";
+            this.button1.Text = "Input - Vaio";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // numUpDownVol1
             // 
-            this.textBox1.Location = new System.Drawing.Point(114, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(31, 20);
-            this.textBox1.TabIndex = 1;
+            this.numUpDownVol1.Location = new System.Drawing.Point(116, 12);
+            this.numUpDownVol1.Name = "numUpDownVol1";
+            this.numUpDownVol1.Size = new System.Drawing.Size(59, 20);
+            this.numUpDownVol1.TabIndex = 2;
+            this.numUpDownVol1.ValueChanged += new System.EventHandler(this.numUpDownVol1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Input - VaioAux";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // numUpDownVol2
+            // 
+            this.numUpDownVol2.Location = new System.Drawing.Point(116, 41);
+            this.numUpDownVol2.Name = "numUpDownVol2";
+            this.numUpDownVol2.Size = new System.Drawing.Size(59, 20);
+            this.numUpDownVol2.TabIndex = 4;
+            this.numUpDownVol2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(189, 73);
+            this.Controls.Add(this.numUpDownVol2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numUpDownVol1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownVol1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownVol2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numUpDownVol1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numUpDownVol2;
     }
 }
 
--- WinVolumeControler/WinVolumeControler/Form1.cs
+++ WinVolumeControler/WinVolumeControler/Form1.cs
@@ -14,17 +14,72 @@ namespace WinVolumeControler
 {
     public partial class Form1 : Form
     {
+        // ---Variables---
+        string DeviceName = "";
+        CoreAudioDevice InputVaio;
+        CoreAudioDevice InputVaioAux;
+
         public Form1()
         {
             InitializeComponent();
+            iwas();
         }
 
-        public void iwas(int input)
-        {
-            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        /*CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
             Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
-            defaultPlaybackDevice.Volume = input;
+            defaultPlaybackDevice.Volume = (double)numUpDownVol1.Value;*/
 
+        /*########################  2do ########################
+         * Start Voice meeter BANANA
+         * Last Volume / Default Vol if no last Volume (RegistryCurrentUser)
+         * Hotkeys 2per Device -> +Vol,-Vol, 
+         * onchanged (numUDInputVaio){InputVaio.Volume = numUDInputVaio.Value}
+         * 
+         * 
+         *###################### 2do End ########################*/
+
+        public void iwas()
+        {
+            CoreAudioController controller = new CoreAudioController();
+            foreach (CoreAudioDevice device in controller.GetPlaybackDevices())
+            {
+                // Console.WriteLine(device.FullName + " " + device.InterfaceName);
+                DeviceName = DeviceName + device.InterfaceName + "\n\n";
+
+                if (device.InterfaceName == "VB-Audio VoiceMeeter VAIO")
+                {
+                    InputVaio = device;
+                }
+                else if (device.InterfaceName == "VB-Audio VoiceMeeter AUX VAIO")
+                {
+                    InputVaioAux = device;
+                }
+            }
+            Debug.WriteLine(DeviceName);
+
+            //initial Value
+            numUpDownVol1.Value = 30;
+            numUpDownVol2.Value = 30;
+
+            InputVaio.Volume = 30;
+            InputVaioAux.Volume = 30;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            iwas();
+        }
+
+        private void numUpDownVol1_ValueChanged(object sender, EventArgs e)
+        {
+            InputVaio.Volume = (double)numUpDownVol1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            InputVaioAux.Volume = (double)numUpDownVol2.Value;
         }
     }
 }
