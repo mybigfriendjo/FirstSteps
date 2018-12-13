2bdd759194a6103faa9d721be7ef700c0123144d
Home <gr4ndpa@hotmail.com>
2018-12-13 01:03:08 +0100

    Fixed Volume
    Public CLass to internal

--- WinVolumeControlerAdvanced/WinVolumeControlerAdvanced/Program.cs
+++ WinVolumeControlerAdvanced/WinVolumeControlerAdvanced/Program.cs
@@ -17,7 +17,6 @@ namespace WinVolumeControler
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Application.Exit();
         }
     }
 }
