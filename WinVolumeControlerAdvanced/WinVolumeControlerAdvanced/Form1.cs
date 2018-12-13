884951b5eeed4fa72725232c9ecb5fab0363b46e
Alpin <rpci9076@A860001E>
2018-12-13 14:59:16 +0100

    rewrote setVolume,integrated setVolume() into Hotkeys
    -rewrote setVolume
    >>to be able to Change Volume for >>multiple Devices
    -integrated setVolume into Hotkeys

--- WinVolumeControlerAdvanced/WinVolumeControlerAdvanced/Form1.cs
+++ WinVolumeControlerAdvanced/WinVolumeControlerAdvanced/Form1.cs
@@ -16,19 +16,34 @@ namespace WinVolumeControler
         private bool controlPressed;
         private bool altPressed;
 
+        // ---Dll Imports---
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+
         public Form1()
         {
             hook.OnKeyPressed += hook_KeyPressed;
             hook.OnKeyUnpressed += hook_KeyUnpressed;
             hook.HookKeyboard();
+
             this.WindowState = FormWindowState.Minimized;
             this.ShowInTaskbar = false;
-
-            setVolume();
             
         }
 
-        //*########################  2do ########################
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            ////setVolume();
+            //Application.Exit();
+        }
+
+        // *########################  2do ########################
         // * 
         // *###################### 2do End ########################
 
@@ -65,28 +80,42 @@ namespace WinVolumeControler
                 case Keys.LMenu:
                     altPressed = true;
                     break;
-                case Keys.F9:
+                case Keys.F9: //App1 lower Vol
                     if (controlPressed && altPressed)
                     {
-                        currentVolume = controller.DefaultPlaybackDevice.Volume - 10;
-                        if (currentVolume < 0)
-                        {
-                            currentVolume = 0;
-                        }
+                        //currentVolume = controller.DefaultPlaybackDevice.Volume - 10;
+                        //if (currentVolume < 0)
+                        //{
+                        //    currentVolume = 0;
+                        //}
 
-                        controller.DefaultPlaybackDevice.Volume = currentVolume;
+                        //setVolume("StarCraft II", "StarCraft II", "decrease");
+                        MessageBox.Show("F9");
                     }
                     break;
-                case Keys.F10:
+                case Keys.F10: //App1 raise Vol
                     if (controlPressed && altPressed)
                     {
                         currentVolume = controller.DefaultPlaybackDevice.Volume + 10;
-                        if (currentVolume > 100)
-                        {
-                            currentVolume = 100;
-                        }
+                        //if (currentVolume > 100)
+                        //{
+                        //    currentVolume = 100;
+                        //}
 
-                        controller.DefaultPlaybackDevice.Volume = currentVolume;
+                        //setVolume("StarCraft II", "StarCraft II", "raise");
+                        MessageBox.Show("F10");
+                    }
+                    break;
+                case Keys.F11: //App2 lower Vol
+                    if (controlPressed && altPressed)
+                    {
+                        setVolume("StarCraft II", "StarCraft II", "decrease");
+                    }
+                    break;
+                case Keys.F12: //App2 raise Vol
+                    if (controlPressed && altPressed)
+                    {
+                        setVolume("StarCraft II", "StarCraft II", "raise");
                     }
                     break;
             }
@@ -99,16 +128,11 @@ namespace WinVolumeControler
 
         // ################################ (End) ofTheHook ################################
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
-        static void setVolume()
+        static void setVolume(string programmClass, string windowName, string changeAppVolume)
         {
-            //var hWnd = FindWindow("SpotifyMainWindow", "Spotify");
-            var hWnd = FindWindow("StarCraft II", "StarCraft II");
+            var hWnd = FindWindow(programmClass, windowName);
             if (hWnd == IntPtr.Zero)
                 return;
 
@@ -117,8 +141,47 @@ namespace WinVolumeControler
             if (pID == 0)
                 return;
 
-            VolumeMixer.SetApplicationVolume((int)pID, 50f);
+            float newApplicationVolume = -1;            //float? is a float accepting also NULL as value exists for every Datatype (except string which can be NULL allready)
+            if ( changeAppVolume == "raise" ){
+                if (VolumeMixer.GetApplicationVolume((int)pID) <= 95)
+                {
+                    newApplicationVolume = (float)VolumeMixer.GetApplicationVolume((int)pID) + 5;
+                }
+                else
+                {
+                    newApplicationVolume = 100f;
+                }
+            }
+            else if ( changeAppVolume == "decrease" )
+            {
+                newApplicationVolume = (float)VolumeMixer.GetApplicationVolume((int)pID) - 5;
+            }
+            if (newApplicationVolume == -1)
+            {
+                VolumeMixer.SetApplicationVolume((int)pID, newApplicationVolume);
+            }
         }
-    }      
+    }
 }
-    
+
+
+
+/*
+
+static void setVolume()
+{
+    var hWnd = FindWindow("StarCraft II", "StarCraft II");
+    if (hWnd == IntPtr.Zero)
+        return;
+
+    uint pID;
+    GetWindowThreadProcessId(hWnd, out pID);
+    if (pID == 0)
+        return;
+
+    VolumeMixer.SetApplicationVolume((int)pID, 50f);
+}
+
+
+
+*/
