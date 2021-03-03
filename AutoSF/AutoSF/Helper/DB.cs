ee443459a34ec5f1aa1845abae054bafc6252635
Alpin <gr4ndpa@hotmail.com>
2021-03-03 17:06:28 +0100

    Create DB.cs
    connection to sqlite DB (currently loaded into a DataTable)

---
+++ AutoSF/AutoSF/Helper/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace AutoSF.Helper {
+    public static class DB {
+
+        private static SQLiteConnection DBConnection;
+        static SQLiteCommand cmd = new SQLiteCommand();
+        public static DataTable dt = new DataTable();
+
+        public static void InitializeDB() {
+            DBConnection = new SQLiteConnection("Data Source=C:\\Users\\gr4nd\\Documents\\GitHub\\FirstSteps\\AutoSF\\AutoSF\\AutoSF.db; Mode=ReadOnly;FailIfMissing=True;Version=3;");  //Version = Sqliteverion
+            DBConnection.Open();
+
+            cmd.Connection = DBConnection;
+            InitializeDBcmd(); //Fills DataTable dt with all data from Table Missions
+        }
+
+        public static SQLiteDataReader InitializeDBcmd(string query = "") {
+            cmd.CommandText = "Select * From Missions" + query;
+            SQLiteDataReader queryResult = cmd.ExecuteReader();
+            dt.Load(queryResult);
+            return queryResult;
+        }
+    }
+}
