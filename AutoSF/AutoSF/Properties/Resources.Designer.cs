fc20e6870931430b9145b94ca336ce3af975e89d
Alpin <gr4ndpa@hotmail.com>
2021-03-08 10:05:48 +0100

    changed resources

--- AutoSF/AutoSF/Properties/Resources.Designer.cs
+++ AutoSF/AutoSF/Properties/Resources.Designer.cs
@@ -73,9 +73,9 @@ namespace AutoSF.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ImgSpeed30 {
+        internal static System.Drawing.Bitmap Speed30 {
             get {
-                object obj = ResourceManager.GetObject("ImgSpeed30", resourceCulture);
+                object obj = ResourceManager.GetObject("Speed30", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
