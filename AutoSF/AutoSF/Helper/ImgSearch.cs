92ca164faa1753d0bbb03fcfecc9adc0a5d44abc
Alpin <gr4ndpa@hotmail.com>
2021-03-25 19:22:21 +0100

    just format update

--- AutoSF/AutoSF/Helper/ImgSearch.cs
+++ AutoSF/AutoSF/Helper/ImgSearch.cs
@@ -9,11 +9,11 @@ using System.Windows.Forms;
 namespace AutoSF.Helper {
     public static class ImgSearch {
 
-        //[DllImport("ImageSearchDLL.dll")]
+        [DllImport(@"C:\ImageSearchDLL.dll")]
+        static extern IntPtr ImageSearch(int x, int y, int right, int bottom, [MarshalAs(UnmanagedType.LPStr)] string imagePath);
 
-        
 
-        public static String[] UseImageSearch(string IMGPath, string tolerance) {
+        public static String[] UseImageSearch(string IMGPath, string tolerance,int xUpperLeftCoordinate = -1920, int yUpperLeftCoordinate = 270,int xLowerRightCoordinate = 0,int yLowerRightCoordinate = 540) {
 
             int right = Screen.PrimaryScreen.WorkingArea.Right;
             int bottom = Screen.PrimaryScreen.WorkingArea.Bottom;
@@ -22,16 +22,12 @@ namespace AutoSF.Helper {
 
             IntPtr result;
             if(MainWindow.CurrentHostName == "VMgr4ndpa") {
-                [DllImport(@"C:\ImageSearchDLL.dll")]
-                static extern IntPtr ImageSearch(int x, int y, int right, int bottom, [MarshalAs(UnmanagedType.LPStr)] string imagePath);
-
-                result = ImageSearch(0, 270, right, 540, IMGPath); //searchArea is in between x 0,and y 270, goes to x 1080,goes to y 540
+                xUpperLeftCoordinate += 1920;
+                xLowerRightCoordinate += 1920;
+                result = ImageSearch(xUpperLeftCoordinate, yUpperLeftCoordinate, xLowerRightCoordinate, yLowerRightCoordinate, IMGPath); //searchArea is in between x 0,and y 270, goes to x 1920,goes to y 540
             }
             else {
-                [DllImport(@"C:\ImageSearchDLL.dll")]
-                static extern IntPtr ImageSearch(int x, int y, int right, int bottom, [MarshalAs(UnmanagedType.LPStr)] string imagePath);
-
-                result = ImageSearch(-1920, 270, right, 540, IMGPath);
+                result = ImageSearch(xUpperLeftCoordinate, yUpperLeftCoordinate, xLowerRightCoordinate, yLowerRightCoordinate, IMGPath);
             }
             String res = Marshal.PtrToStringAnsi(result);
 
@@ -41,9 +37,9 @@ namespace AutoSF.Helper {
             //0->found, 1->x, 2->y, 3->image width, 4->image height;        
 
             // Then, you can parse it to get x and y:
-            int x; int y;
-            int.TryParse(data[1], out x);
-            int.TryParse(data[2], out y);
+            //int x; int y;
+            //int.TryParse(data[1], out x);
+            //int.TryParse(data[2], out y);
             //Console.WriteLine(x.ToString() + ", " + y.ToString());
 
             return data;
