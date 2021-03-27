396a34a67246d82272b31637e153e7eb8c210651
Alpin <gr4ndpa@hotmail.com>
2021-03-28 00:19:35 +0100

    Dissmiss Soldier completed

--- AutoSF/AutoSF/DismissSoldier.cs
+++ AutoSF/AutoSF/DismissSoldier.cs
@@ -21,97 +21,115 @@ namespace AutoSF {
         }
 
         private static void DissmissMissingSoldiers() {
-            if(CheckforTruppkameradenliste() == 1) {
-                MouseActions.SingleClickAtPosition(-122, 925);
-                if(CheckforFilterScreen() == 1) {
+            StopSoldierDismiss = false;
+            if(StopSoldierDismiss == false && CheckforTruppkameradenliste() == 1) {
+                if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
+                    log.Debug("Dismissing Soldiers interrupted by user at: CheckforTruppkameradenliste");
+                    StopSoldierDismiss = true;
+                    return;
+                }
+                MouseActions.SingleClickAtPosition(-122, 925); //open Filtermenu
+                if(StopSoldierDismiss == false && CheckforFilterScreen() == 1) {
                     MouseActions.SingleClickAtPosition(-742, 218); //nicht verfügbar
-                    MainWindow.Sleep(150);
-                    MouseActions.SingleClickAtPosition(-551, 611); // activate 1*
-                    MainWindow.Sleep(150);
-                    MouseActions.SingleClickAtPosition(-742, 218); // activate 2*
-                    MainWindow.Sleep(150);
-                    MouseActions.SingleClickAtPosition(-1738, 209); // Close FilterScreen
                     MainWindow.Sleep(250);
-                    if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
-                        log.Debug("Dismissing Soldiers interrupted by user at: CheckforTruppkameradenliste");
-                        StopSoldierDismiss = true;
-                        return;
-                    }
-                    string[] ImageLocation = ImgSearch.UseImageSearch(MainWindow.ResourcesPath + "booster.png", "120", -1920,748,0,120);
-                    if (StopSoldierDismiss == false && ImageLocation != null && ImageLocation.Length > 0) {
-                        int.TryParse(ImageLocation[1], out int x);
-                        int.TryParse(ImageLocation[1], out int y);
-                        MouseActions.SingleClickAtPosition(x - 100, y - 200); //upperleft corner of picture (which is in the "Bergung" Button) -> to middle of soldier screen
-                        if(CheckforSoldierDissmissScreen() == 1) {
-                            MouseActions.SingleClickAtPosition(-1834, 997); // Dissmiss Soldier
-                            if(CheckforAcceptDissmissScreen() == 1) {
-                                MouseActions.SingleClickAtPosition(-421, 811); //"Accept" Dismiss Button
-                            }
-                            else {
-                                StopSoldierDismiss = true;
-                                return;
-                            }
-                        }
-                        else {
+                    MouseActions.SingleClickAtPosition(-551, 611); // activate 1*
+                    MainWindow.Sleep(250);
+                    MouseActions.SingleClickAtPosition(-446, 612); // activate 2*
+                    MainWindow.Sleep(250);
+                    MouseActions.SingleClickAtPosition(-1738, 209); // Close FilterScreen
+                    MainWindow.Sleep(350);
+                    int AmountOfLoops = 0;
+                    while(AmountOfLoops++ < 20) {
+                        if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
+                            log.Debug("Dismissing Soldiers interrupted by user at: CheckforTruppkameradenliste");
                             StopSoldierDismiss = true;
                             return;
                         }
-                    }
-                    else if(StopSoldierDismiss == false && ImageLocation == null) {
-                        //scroll 3boosters
-                        int i = 0;
-                        while(StopSoldierDismiss == false && i < 3) {
-                            if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
-                                log.Debug("Dismissing Soldiers interrupted by user at: CheckforTruppkameradenliste");
-                                StopSoldierDismiss = true;
-                                return;
-                            }
-                            int moveToX = 0;
-                            if(MainWindow.CurrentHostName == "VMgr4ndpa") {
-                                MouseActions.SetCursorPos(1868, 484);
-                                moveToX = 1715;
-                                MouseActions.LeftMouseDown();
-                                while(moveToX > 48 && StopSoldierDismiss == false) {
-                                    if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
-                                        log.Debug("AutoMission interrupted by user at: selecting Booster");
+                        string[] ImageLocation = ImgSearch.UseImageSearch(MainWindow.ResourcesPath + "berger.png", "140", -1920, 748, 1920, 890);
+                        if(StopSoldierDismiss == false && ImageLocation != null && ImageLocation.Length > 0) {
+                            int.TryParse(ImageLocation[1], out int x);
+                            int.TryParse(ImageLocation[2], out int y);
+                            MouseActions.SingleClickAtPosition(x - 100, y - 200); //upperleft corner of picture (which is in the "Bergung" Button) -> to middle of soldier screen
+                            if(CheckforSoldierDissmissScreen() == 1) {
+                                MouseActions.SingleClickAtPosition(-1834, 997); // Dissmiss Soldier
+                                if(CheckforAcceptDissmissScreen() == 1) {
+                                    MouseActions.SingleClickAtPosition(-421, 811); //"Accept" Dismiss Button
+                                    if(StopSoldierDismiss == false && CheckforTruppkameradenliste() == 1) {
+                                    }
+                                    else {
                                         StopSoldierDismiss = true;
                                         return;
                                     }
-                                    moveToX -= 90;
-                                    MouseActions.SetCursorPos(moveToX, 484);
-                                    MainWindow.Sleep(34);
+                                }
+                                else {
+                                    StopSoldierDismiss = true;
+                                    return;
                                 }
                             }
                             else {
-                                MouseActions.SetCursorPos(-52, 557);
-                                moveToX = -215;
-                                MouseActions.LeftMouseDown();
-                                while(moveToX > -1872 && StopSoldierDismiss == false) {
-                                    if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
-                                        log.Debug("AutoMission interrupted by user at: selecting Booster");
-                                        StopSoldierDismiss = true;
-                                        return;
-                                    }
-                                    moveToX -= 90;
-                                    MouseActions.SetCursorPos(moveToX, 484);
-                                    MainWindow.Sleep(34);
-                                }
-                            }
-                            MainWindow.Sleep(150);
-                            MouseActions.LeftMouseUp();
-                            i++;
-                            if(StopSoldierDismiss == false && ImgSearch.UseImageSearch(MainWindow.ResourcesPath + "booster.png", "120", -1920, 748, 0, 120) != null) {
-                                break;
-                            }
-                            else if(StopSoldierDismiss == true) {
+                                StopSoldierDismiss = true;
                                 return;
                             }
                         }
+                        else if(StopSoldierDismiss == false && ImageLocation == null) {
+                            //scroll 3boosters
+                            int i = 0;
+                            while(StopSoldierDismiss == false && i < 3) {
+                                if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
+                                    log.Debug("Dismissing Soldiers interrupted by user at: CheckforTruppkameradenliste");
+                                    StopSoldierDismiss = true;
+                                    return;
+                                }
+                                int moveToX = 0;
+                                if(MainWindow.CurrentHostName == "VMgr4ndpa") {
+                                    MouseActions.SetCursorPos(1868, 484);
+                                    moveToX = 1715;
+                                    MouseActions.LeftMouseDown();
+                                    while(moveToX > 48 && StopSoldierDismiss == false) {
+                                        if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
+                                            log.Debug("AutoMission interrupted by user at: selecting Booster");
+                                            StopSoldierDismiss = true;
+                                            return;
+                                        }
+                                        moveToX -= 90;
+                                        MouseActions.SetCursorPos(moveToX, 484);
+                                        MainWindow.Sleep(34);
+                                    }
+                                }
+                                else {
+                                    MouseActions.SetCursorPos(-52, 557);
+                                    moveToX = -215;
+                                    MouseActions.LeftMouseDown();
+                                    while(moveToX > -1872 && StopSoldierDismiss == false) {
+                                        if(Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftAlt) && Keyboard.IsKeyDown(Key.NumPad0)) {
+                                            log.Debug("AutoMission interrupted by user at: selecting Booster");
+                                            StopSoldierDismiss = true;
+                                            return;
+                                        }
+                                        moveToX -= 90;
+                                        MouseActions.SetCursorPos(moveToX, 484);
+                                        MainWindow.Sleep(34);
+                                    }
+                                }
+                                MainWindow.Sleep(150);
+                                MouseActions.LeftMouseUp();
+                                i++;
+                                if(StopSoldierDismiss == false && ImgSearch.UseImageSearch(MainWindow.ResourcesPath + "berger.png", "140", -1920, 748, 1920, 890) != null) {
+                                    break;
+                                }
+                                else if(StopSoldierDismiss == true) {
+                                    return;
+                                }
+                            }
 
+                        }
+                        else if(StopSoldierDismiss == true) {
+                            return;
+                        }
                     }
-                    else if(StopSoldierDismiss == true) {
-                        return;
-                    }
+                    log.Debug("Loops finished.");
+                    StopSoldierDismiss = true;
+                    return;
                 }
                 else {
                     StopSoldierDismiss = true;
@@ -134,9 +152,9 @@ namespace AutoSF {
                     StopSoldierDismiss = true;
                     return -1;
                 }
-                if(PixelFinder.SearchStaticPixel(417, 948, "#60A0B9")) { Score++; } // Button Verteidigung
+                if(PixelFinder.SearchStaticPixel(417, 948, "#448FAD")) { Score++; } // Button Verteidigung
                 if(PixelFinder.SearchStaticPixel(906, 122, "#0F2D51")) { Score++; } // Background of Truppkameraden-liste
-                if(PixelFinder.SearchStaticPixel(1826, 12, "#FEFEFE")) { Score++; } // x in the upper right to close window
+                if(PixelFinder.SearchStaticPixel(1828, 125, "#FFFFFF")) { Score++; } // x in the upper right to close window
                 LoopGarbageCollector.ClearGarbageCollector();
                 if(++i > 25) {
                     log.Debug("Truppkameradenliste couldnt be found in time:");
