c4d75553a4da38d0f593429914e6a53b24050733
Alpin <gr4ndpa@hotmail.com>
2021-04-27 17:43:55 +0200

    Added seperate Window for Grid

---
+++ AutoSF/AutoSF/SecondWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using static AutoSF.AutoMission;
+using System.Data;
+
+namespace AutoSF {
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window, IAutomissionListener {
+        public SecondWindow() {
+            InitializeComponent();
+        }
+
+        public void Update(DataTable dtInput) { //For multiple Threads
+            if(!Dispatcher.CheckAccess()) {
+                Dispatcher.Invoke(new InvokeUpdate(Update), dtInput);
+                return;
+            }
+            else {
+                DataGridTableCacheDB.ItemsSource = dtInput.DefaultView;
+            }
+        }
+        private delegate void InvokeUpdate(DataTable table);
+    }
+}
